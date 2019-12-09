2060cbb00607c77625e091b638a725b79e2a0d03
noopurs2 <Saaketh197>
2019-12-09 15:22:02 -0600

    new changes with buttons

--- app/src/main/java/com/example/accomplist/FragmentStory.java
+++ app/src/main/java/com/example/accomplist/FragmentStory.java
@@ -2,11 +2,16 @@ package com.example.accomplist;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -27,5 +32,20 @@ public class FragmentStory extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_story, container, false);
+    }
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        final NavController navController = Navigation.findNavController(view);
+
+        Button homeButton = view.findViewById(R.id.homeBtn);
+        homeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                navController.navigate(R.id.action_fragmentStory_to_fragmentHome);
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/accomplist/FragmentTask.java
+++ app/src/main/java/com/example/accomplist/FragmentTask.java
@@ -2,11 +2,16 @@ package com.example.accomplist;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -25,5 +30,20 @@ public class FragmentTask extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_task, container, false);
+    }
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        final NavController navController = Navigation.findNavController(view);
+
+        Button bacButton = view.findViewById(R.id.bacBtn);
+        bacButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                navController.navigate(R.id.action_fragmentTask_to_fragmentHome);
+            }
+        });
+
     }
 }
